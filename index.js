e420da14ef747e9949e4237cc704e947f900c96f
Connor Adams <connorvadams@gmail.com>
2023-09-11 21:20:32 -0600

    Adds functions to calculate distance in ft and fare

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const hqLocation = 42;
 
-function distanceFromHqInBlocks(distanceInt) {
-    return distanceInt < hqLocation ? hqLocation - distanceInt : distanceInt - hqLocation;
+function distanceFromHqInBlocks(startingBlockInt) {
+    return startingBlockInt <= hqLocation ? hqLocation - startingBlockInt : startingBlockInt - hqLocation;
 };
 
-function distanceFromHqInFeet(inputValue) {
-    distanceFromHqInBlocks(inputValue);
-
+function convertBlocksToFt(blockInt) {
+    return blockInt * 264;
 }
 
+function distanceFromHqInFeet(startingBlockInt) {
+   return convertBlocksToFt(distanceFromHqInBlocks(startingBlockInt));
+};
+
+function distanceTravelledInFeet(startingBlockInt, endingBlockInt) {
+    return convertBlocksToFt(startingBlockInt <= endingBlockInt ? endingBlockInt - startingBlockInt : startingBlockInt - endingBlockInt);
+}
+
+function calculatesFarePrice(startingBlockInt, endingBlockInt) {
+    let distanceTravelled = distanceTravelledInFeet(startingBlockInt, endingBlockInt);
+    if (distanceTravelled <= 400) {
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * .02;
+    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
