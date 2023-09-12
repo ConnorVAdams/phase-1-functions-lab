5575b72aa73d178463c4e3fb117a86aade3abc4e
Connor Adams <connorvadams@gmail.com>
2023-09-11 20:29:25 -0600

    Creates function that returns distance of ride in blocks.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+const hqLocation = 42;
+
+function distanceFromHqInBlocks(distanceInt) {
+    return distanceInt < hqLocation ? hqLocation - distanceInt : distanceInt - hqLocation;
+};
+
+function distanceFromHqInFeet(inputValue) {
+    distanceFromHqInBlocks(inputValue);
+
+}
+
